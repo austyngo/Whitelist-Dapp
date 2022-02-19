68c0c8f138d2e23d227cb85c130b2393901554a8
Austin Ngo <austinngo38@gmail.com>
2022-02-18 22:36:29 -0800

    update imports

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/Head";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
